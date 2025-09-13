4bd07308ebe75b333c77be74e748df588f63a2ba
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-09-13 14:18:38 +0300

    Create FloatDouble2.java

---
+++ Java/FloatDouble2.java
@@ -0,0 +1,77 @@
+class Main { 
+
+    public static void main(String[] args) { 
+
+        //These are comments and they are ignored by the compiler 
+
+         
+
+        int score = 10;
+        score++;            //increment operator 
+        System.out.println(score); 
+
+         
+
+        score--;            //decrement operator 
+        System.out.println(score); 
+
+         
+
+         
+
+        int x = 10; 
+        int y = 3; 
+
+         
+
+        int answer = x + y;
+        System.out.println("Add = " + answer); //concatenation + operator with text 
+
+         
+
+        answer = x - y;
+        System.out.println("Minus = " + answer); 
+
+         
+
+        //Multiply
+        answer = x * y;
+        System.out.println("Multiply = " + answer);
+
+         
+
+        //Divide
+        answer = x / y;
+        System.out.println("Divide = " + answer);// logical error ? 
+         
+
+         
+        //Divide with double
+        double ans = x/y;
+        System.out.println("Divide = " + ans);// still error? 
+
+         
+
+        //double solution
+         
+
+
+        System.out.println(); 
+
+         
+
+        
+
+        //now do the same thing with floats 
+
+         
+
+        System.out.println(); 
+
+         
+
+         
+
+    } 
+
+} 
