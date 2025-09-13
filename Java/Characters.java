5f34dbc9a3b36d85c5baa78c8f3a8e2ed8474730
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-09-13 14:57:20 +0300

    Create Characters.java

---
+++ Java/Characters.java
@@ -0,0 +1,51 @@
+class Main { 
+
+    public static void main(String[] args) { 
+
+//Char 
+//https://www.rapidtables.com/code/text/unicode-characters.html 
+
+  
+
+        char c = 'A';     //regular 
+        System.out.println(c); 
+         
+
+        c = '\u0041';     //unicode
+        System.out.println(c); 
+         
+
+        c = 65;     //decimal 
+        System.out.println(c); 
+         
+
+ 
+
+        //================================= 
+
+System.out.println("============="); 
+ 
+
+         
+
+        //accepts ++/-- operators 
+        c++; 
+        System.out.println(c);          
+
+        c='9'; 
+        System.out.println(c);          
+
+        c++; 
+        System.out.println(c);//why not 10?  
+
+         
+
+        //useful 
+        System.out.println("this is line1 \n this is line2"); 
+        System.out.println("this is a tab \t got it?"); 
+
+         
+
+    } 
+
+} 
