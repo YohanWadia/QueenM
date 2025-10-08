4214c75e7d6bf71c5fee0d31f2a751c87b3389f8
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-10-08 13:53:29 +0300

    Create game_dev_2.dart

---
+++ Flutter/GameConcepts1/game_dev_2.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+ 
+void main() {
+  runApp(MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Move Square Game',
+      home: MyHomePage(),
+    );
+  }
+}
+ 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+ 
+class _MyHomePageState extends State<MyHomePage> {
+ double x=0,y=0;
+  
+  //=====================================
+  
+  moveRight(){
+    
+  }
+  
+  moveLeft(){
+    
+  }
+  
+  moveUp(){
+    
+  }
+  
+  moveDown(){
+    
+  } 
+  
+  moveRightUp(){
+    
+  }
+  
+   moveRightDown(){
+    
+  }
+  
+  moveLeftDown(){
+    
+  }
+  
+  moveLeftUp(){
+    
+  }
+  
+ 
+ //=====================================
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Move Square Game'),
+      ),
+      body:
+      Center(child:
+      Column(//mainAxisAlignment:MainAxisAlignment.center,
+        children: <Widget>[
+        Container(
+          color: Colors.amber,
+          width: 400,
+          height: 400,
+          child: CustomPaint(
+            painter: OpenPainter(x,y),
+          ),
+        ),
+        Row( mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+          children:[
+          ElevatedButton(
+          onPressed:(){moveLeft(); setState((){}); },
+          child:Text("<<")
+          ),
+        ElevatedButton(
+          onPressed:(){moveRight(); setState((){});},
+          child:Text(">>")
+          ),
+        ElevatedButton(
+          onPressed:(){moveUp(); setState((){});},
+          child:Text("UP")
+          ),
+        ElevatedButton(
+          onPressed:(){moveDown(); setState((){});},
+          child:Text("Down")
+          ),
+        ElevatedButton(
+          onPressed:(){moveRightUp(); setState((){}); },
+          child:Text("↗")),
+        ElevatedButton(
+          onPressed:(){moveRightDown(); setState((){});},
+          child:Text("↘")
+          ),
+        ElevatedButton(
+          onPressed:(){moveLeftDown(); setState((){});},
+          child:Text("↙")
+          ),
+        ElevatedButton(
+          onPressed:(){moveLeftUp(); setState((){});},
+          child:Text("↖")
+          ),    
+        ])
+        
+      ]),
+             )
+    );
+  }
+}
+ 
+class OpenPainter extends CustomPainter {
+  double x,y;
+  OpenPainter(this.x,this.y);
+ 
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Color(0xff995588)
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset(x,y) & const Size(50, 50), paint1);
+  }
+ 
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
