f3187e66f6598bebf475bcba54a7c182fd30012a
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-10-23 23:05:58 +0300

    Create ifelse.java

---
+++ Java/ifelse.java
@@ -0,0 +1,32 @@
+class Main {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        if(age>=20){
+            System.out.println("You are allowed to drink");
+        }
+        else{
+            System.out.println("You are NOT allowed to drink");
+        }
+        
+        
+        int speed = 110;
+        if(speed>=100){
+            System.out.println("Pay Fine");
+        }
+        else if(speed>=80){
+            System.out.println("Warning");
+        }
+        else{
+            System.out.println("Perfect Driving");
+        }
+        
+        
+        int x = 10;
+        if(x==10){
+            System.out.println("the value is equal to 10");
+        }
+        
+        
+    }
+}
