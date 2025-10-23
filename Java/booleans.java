16891697957883d4c0a6d768616984046f77333e
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-10-23 23:49:51 +0300

    Create booleans.java

---
+++ Java/booleans.java
@@ -0,0 +1,54 @@
+class Main {
+    public static void main(String[] args) {
+        
+         //BOOLEAN
+        
+        boolean isOn = false;
+        System.out.println(isOn);
+        
+        isOn = true;
+        System.out.println(isOn);
+        
+        
+        //===================print Lamps status
+        // if(condition){
+        //     //what should happen
+        // }
+        // else if(condition){
+        //     //what should happen
+        // }
+        
+        
+        //====================== toggle switch for the Lamp
+        // if(condition){
+        //     //what should happen
+        // }
+        // else if(condition){
+        //     //what should happen
+        // }
+        
+        
+        //========================negatives
+        int x = 11;
+        if(x!=10){
+            System.out.println("value is not equal to 10");
+        }
+        
+        boolean isMember = true;
+        if(isMember){
+            System.out.println("Allow in the club");
+        }
+        
+        isMember = false;
+        if(!isMember){
+            System.out.println("NOT allowed in the club");
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
