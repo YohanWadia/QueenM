0a2d8ae677b49bc5bb324630e124e0d5b79a7f21
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2025-10-24 16:22:50 +0300

    Create Strings2.java

---
+++ Java/Strings2.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+        //Strings 2
+        //Return Values
+        
+        //Empty string vs non initialized vs null
+        
+        String myName="Yohan Xerxes Wadia"; 
+        int len = myName.length();
+        System.out.println(len);
+        
+        System.out.println("****************");
+        
+        String str1, str2, str3; 
+        str1 = "abcd";
+        str2 = "efgh";
+
+        str3 = str1.concat(str2);
+        System.out.println(str3);
+        
+    }
+}
