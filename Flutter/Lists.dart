898dad8d8d9c7f8027e3e722f560c354819b1ac1
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2026-02-27 23:52:42 +0200

    Create Lists.dart

---
+++ Flutter/Lists.dart
@@ -0,0 +1,11 @@
+Future<void> main() async {
+  
+  List<int> numbers = [1,2,3,4,5];
+  List<String> names = ["Yohan", "Aistis", "Igor", "Jonas", "Lukas"];
+  
+  for (int i = 0; i < 5; i++) {
+    print(numbers[i]);
+    print(names[i]);
+    
+  }
+}
