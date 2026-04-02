6cd87f13d1c5d97abd6b9b9190a3bc8002538f30
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2026-04-02 14:41:10 +0300

    Update NestedLoops.java

--- Java/Loops/NestedLoops.java
+++ Java/Loops/NestedLoops.java
@@ -1,11 +1,11 @@
 public class NestedLoops {
     static void main() {
 
-        for(int i = 1; i<11; i++){
+        for(int y = 1; y<11; y++){
             System.out.println("===== i : " + i);
 
-            for (int j = 1; j < 11; j++) {
-                System.out.println("j-" + j);
+            for (int x = 1; x < 11; x++) {
+                System.out.println("x-" + j);
             }// j loop
 
 
