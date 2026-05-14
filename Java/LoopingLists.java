fea88e37061c535f59f881fc0842b501264111ec
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2026-05-14 09:04:36 +0300

    Create LoopingLists.java

---
+++ Java/LoopingLists.java
@@ -0,0 +1,25 @@
+List<String> namesList = new ArrayList<>();
+    namesList.add("");
+    namesList.add("");
+    namesList.add("");
+    namesList.add("");
+    namesList.add("");
+    namesList.add("");
+
+    System.out.println(namesList.get(0));
+    System.out.println(namesList);
+
+    System.out.println("==========================");
+
+    List<Integer> scoresList = new ArrayList<>();
+    scoresList.add(0);
+    scoresList.add(0);
+    scoresList.add(0);
+    scoresList.add(0);
+    scoresList.add(0);
+    scoresList.add(0);
+
+    System.out.println(scoresList.get(5));
+    System.out.println(scoresList);
+
+    System.out.println("==========================");
