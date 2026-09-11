f6c90cd510cc98d96173fffb1f55be508a2bd8f6
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2026-09-11 15:33:18 +0300

    Create Constructors.java

---
+++ Java/Constructors.java
@@ -0,0 +1,12 @@
+// The Blueprint (Class)
+class Student {
+    // Properties (State)
+    int id;
+    String name;
+    int age;
+
+    // Behavior (Action) Method
+    public void displayInfo() {
+        System.out.println("ID: " + id + " | Name: " + name + " | Age: " + age);
+    }
+}
